c57ab8d6b68feb59bc191123ae3fe67cd63d8b77
yuvi595 <jinshang267@gmail.com>
2024-09-27 10:31:48 +0530

    java

---
+++ expenseTracker.java
@@ -0,0 +1,12 @@
+package expenseTracker;
+
+public class expenseTracker {
+	public static void main(String[] args) {
+		byte a = 123;
+		byte b = 10;
+		byte c = (byte)(a+b);
+		System.out.println(c);
+		
+	}
+
+}
